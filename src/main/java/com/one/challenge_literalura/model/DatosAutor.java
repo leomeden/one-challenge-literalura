c2080845a98497c2e4dc8152d5cbcc7d28ae17ee
leomeden <leonardomeden@gmail.com>
2024-05-28 16:37:13 -0300

    Agregada propiedad año de muerte en datosautor

--- src/main/java/com/one/challenge_literalura/model/DatosAutor.java
+++ src/main/java/com/one/challenge_literalura/model/DatosAutor.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DatosAutor(
         @JsonAlias("name") String nombre,
-        @JsonAlias("birth_year") String fechaDeNacimiento
+        @JsonAlias("birth_year") String fechaDeNacimiento,
+        @JsonAlias("death_year") String fechaDeMuerte
         ) {
 }
